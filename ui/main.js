1bb245615f13d0d7093746134ccbb9c0daccc43b
Alec Jordan <alecbjordan@gmail.com>
2021-04-30 12:29:03 -0400

    Adding news/twitter functionality. Layout needs refinement.

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,17 @@
 const app = Vue.createApp({
     data() {
         return {
-
+            news_items: [
+                {'id': 1,
+                 'text': 'This is a news item!',
+                 'time': '13 hours ago'},
+                 {'id': 2,
+                 'text': 'And another one!',
+                 'time': '13 hours ago'},
+                 {'id': 3,
+                 'text': 'Third one!',
+                 'time': '13 hours ago'}
+            ]
         }
     }
 })
